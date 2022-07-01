5b4a5a9e9bbdae61aa9676fc293a09ca1c1ab157
XZ <odufeso1@gmail.com>
2022-07-01 13:18:53 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hex;
+
+	for (hex = 0x30; hex <= 0x39; hex++)
+		putchar(hex);
+	for (hex = 0x61; hex <= 0x65; hex++)
+		putchar(hex);
+	putchar('\n');
+	return (0);
+}
