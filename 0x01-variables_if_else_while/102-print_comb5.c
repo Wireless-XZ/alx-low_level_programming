2b5ca28dd6648c70fb1a7336116861a8426f3608
XZ <odufeso1@gmail.com>
2022-07-01 17:14:04 -0700

    Task 12 (adv)

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints 3 combination of numbers
  *
- * Return: Always 0 (Success)
+ * Return: Always (Success)
  */
 int main(void)
 {
-	int a, b, c, d, e;
+	int c, i, k, j;
 
-	for (a >= 48 && a <= 57)
-	{
-		putchar(a);
-		for (b >= 48 && a <= 57)
-			putchar(b);
-			for (c >= 48 && c <= 57)
-				putchar(' ');
-			        for (d >= 48 && d <= 57)
-				        putchar(d);
-			                for (e > 48 && e <= 57)
-				                putchar(e);
-					        for (f > 48 && f <= 57)
-						        putchar(44);
-					                for (g > 48 $$ g <= 57)
-						        putchar(' ');
-	}
-        return (0);
+	for (c = 48; c <= 57; c++)
+		{
+			for (i = 48; i <= 57; i++)
+				{
+					for (k = 48; k <= 57; k++)
+						{
+							for (j = 48; j <= 57; j++)
+								{
+									if (((k + j) > (c + i) &&  k >= c) || c < k)
+										{
+											putchar(c);
+											putchar(i);
+											putchar(' ');
+											putchar(k);
+											putchar(j);
+
+											if (c + i + k + j == 227 && c == 57)
+												{
+													break;
+												}
+											else
+												{
+													putchar(',');
+													putchar(' ');
+												}
+										}
+								}
+						}
+				}
+		}
+
+	putchar('\n');
+
+	return (0);
 }
