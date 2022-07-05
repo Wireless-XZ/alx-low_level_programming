f4e4980cef86d84182f66f834f6a702d933cda30
XZ <odufeso1@gmail.com>
2022-07-05 13:34:28 -0700

    Bug

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,21 +7,40 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
-
-	for (a = 48; a <= 50; ++a)
+	int a, b, c, d, w, x, y, z;
+	for (a = 48; a <= 49; ++a)
 	{
-		for (b = 48; b <= 51; ++b)
+       		for (b = 48; b <= 57; ++b)
 		{
-			for (c = 48; c <= 53; ++c)
+	       		for (c = 48; c <= 53; ++c)
 			{
-				for (d = 48; d <= 57; ++d)
+		       		for (d = 48; d <= 57; ++d)
 				{
 					_putchar(a);
 					_putchar(b);
 					_putchar(':');
 					_putchar(c);
 					_putchar(d);
+					_putchar('\n');
+
+				}
+			}
+		}
+	}
+
+	for (w = 50; w < 51; ++w)
+	{
+		for (x = 48; x <= 51; ++x)
+		{
+			for (y = 48; y <= 53; ++y)
+			{
+				for (z = 48; z <= 57; ++z)
+				{
+					_putchar(w);
+					_putchar(x);
+					_putchar(':');
+					_putchar(y);
+					_putchar(z);
 					_putchar('\n');
 				}
 			}
