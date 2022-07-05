885fa1b715f5e9cf082b5b3b89bd48047801c206
XZ <odufeso1@gmail.com>
2022-07-05 10:52:54 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Removes all negative sign from an integer
+ * @int: The integer
+ *
+ * Return: Always a positive integer if code runs successfully
+ */
+int _abs(int c)
+{
+	int result;
+
+	if (c < 0)
+		result = (c * -1);
+	else if (c >= 0)
+		result = c;
+
+	return (result);
+}
