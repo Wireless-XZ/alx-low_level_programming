dd90adda8c3c5c9360243e5ee03d53e1c7718b37
XZ <odufeso1@gmail.com>
2022-07-05 13:38:09 -0700

    Betty fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,13 +8,14 @@
 void jack_bauer(void)
 {
 	int a, b, c, d, w, x, y, z;
+
 	for (a = 48; a <= 49; ++a)
 	{
-       		for (b = 48; b <= 57; ++b)
+		for (b = 48; b <= 57; ++b)
 		{
-	       		for (c = 48; c <= 53; ++c)
+			for (c = 48; c <= 53; ++c)
 			{
-		       		for (d = 48; d <= 57; ++d)
+				for (d = 48; d <= 57; ++d)
 				{
 					_putchar(a);
 					_putchar(b);
