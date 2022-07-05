6488c6ed0f25bbd56ca07e72f12b4116c89deb7d
XZ <odufeso1@gmail.com>
2022-07-05 09:42:19 -0700

    Bug Fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,18 @@
 
 /**
  * _islower - checks for lowercase character
+ * @c: The character to be checked
  *
- * Return: Always 1 if character is lowercase
+ * Return: 1 if character is lowercase otherwise 0
  */
 int _islower(int c)
 {
 	int result;
 
-	if (c >= 65 && c <= 90)
-		result = 0;
-	else
+	if (c >= 97 && c <= 122)
 		result = 1;
+	else
+		result = 0;
 
 	return (result);
 }
