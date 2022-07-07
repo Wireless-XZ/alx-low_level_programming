db181871f466ef82837fb276d86a8bdb2b4bbba8
XZ <odufeso1@gmail.com>
2022-07-07 15:51:51 -0700

    Fix

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - contains the body of the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int n = 612852475143, i;
+
+	for (i = 3; i <= 612852475143; i = i + 2)
+	{
+		while ((n % i == 0) && (i != n))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+
+	return (0);
+}
