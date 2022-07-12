0f3b7735de08693476242f0e830ef6269424d1ef
XZ <odufeso1@gmail.com>
2022-07-12 09:16:45 -0700

    Man Task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: The string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	char ch;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		++i;
+	}
+
+	while (i >= 0)
+	{
+		ch = s[i];
+		_putchar(ch);
+		--i;
+	}
+	_putchar('\n');
+}
