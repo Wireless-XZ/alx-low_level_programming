318543b160f53d6b23da4cd397b74e9374e2d907
XZ <odufeso1@gmail.com>
2022-07-18 07:56:05 -0700

    fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,7 +10,7 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	unsigned int i, j;
 
 	for (i = 0; s[i]; i++)
 	{
@@ -20,9 +20,9 @@ char *_strpbrk(char *s, char *accept)
 			{
 				break;
 			}
-			if (accept[j])
-				return (s + i);
 		}
+		if (accept[j])
+			return (s + i);
 	}
 	return (0);
 }
