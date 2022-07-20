1e5c68ba9492e50078f06cb6186d0a98cef90ce9
XZ <odufeso1@gmail.com>
2022-07-20 08:28:29 -0700

    Fixed 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,14 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i])
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i++;
-		_puts_recursion(s + i);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
