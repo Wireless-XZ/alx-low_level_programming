58a4ca371a6cd67535899081b8381d5dc0729203
XZ <odufeso1@gmail.com>
2022-08-15 11:37:46 -0700

    Debugging

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			new->n = n;
 			new->next = temp;
 			last->next = new;
+			i++;
 			return (new);
 		}
 		else if (i + 1 == idx)
