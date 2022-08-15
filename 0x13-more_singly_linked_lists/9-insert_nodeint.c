6c9b683bf20679048426d24fab0fe6196470351a
XZ <odufeso1@gmail.com>
2022-08-15 16:33:30 -0700

    Debugging

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
-	while (temp)
+	while (temp && i <= idx)
 	{
 		if (i + 1 == idx)
 		{
