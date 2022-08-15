2301297d48efe531256e105fd761a741f3a0558c
XZ <odufeso1@gmail.com>
2022-08-15 10:17:49 -0700

    Debugging

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (!head)
+		return;
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
-		*head = NULL;
 		*head = temp;
 	}
 }
