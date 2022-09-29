5fcf3d33d75c3118b3755b58dde812bece3aaaf0
xz <odufeso1@gmail.com>
2022-09-29 09:54:17 -0700

    Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: head node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *dummy = head;
+
+	while (head->next)
+	{
+		head = head->next;
+		free(dummy);
+		dummy = head;
+	}
+	free(dummy);
+}
