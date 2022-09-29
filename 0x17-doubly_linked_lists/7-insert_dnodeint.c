a882c35251b6d7bbc0c555b711b03a766bfb1860
xz <odufeso1@gmail.com>
2022-09-29 13:36:15 -0700

    Task 8

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	else if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -38,10 +37,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		dummy = dummy->next;
 	}
-	/**
-	 *if (i == idx)
-	 *	return (add_dnodeint_end(h, n));
-	 */
 	return (NULL);
 }
 
