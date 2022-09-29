5ef839867ed85e1fa87054fbdba43248186f4bb3
xz <odufeso1@gmail.com>
2022-09-29 14:45:42 -0700

    Bug fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,13 +12,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i;
 	dlistint_t *dummy = *head;
 
-	if (*head == NULL)
-		return (1);
+	if (*head == NULL || head == NULL)
+		return (-1);
 	else if (index == 0)
 	{
 		*head = (*head)->next;
 		if (*head)
 			(*head)->prev = NULL;
+		free(dummy);
 		return (1);
 	}
 	for (i = 0; dummy; i++)
@@ -28,10 +29,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (dummy->next == NULL)
 			{
 				dummy->prev->next = NULL;
+				free(dummy);
 				return (1);
 			}
 			dummy->prev->next = dummy->next;
 			dummy->next->prev = dummy->prev;
+			free(dummy);
 			return (1);
 		}
 		dummy = dummy->next;
