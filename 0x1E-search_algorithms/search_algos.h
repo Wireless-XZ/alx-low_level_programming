7b0899f40f06126bbc4fb65c646581e0e417170d
xz <odufeso1@gmail.com>
2023-02-28 02:53:41 +0100

    Bug fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -55,5 +55,6 @@ int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+void print_array(const int *array, size_t start, size_t end);
 
 #endif
