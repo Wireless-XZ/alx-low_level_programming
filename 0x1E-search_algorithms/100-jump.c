d10b7703186623da3567df90efc43297274838f3
xz <odufeso1@gmail.com>
2023-02-28 01:59:18 +0100

    Updated File

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,37 +1,78 @@
 #include "search_algos.h"
 
 /**
- * jump_search - function that searches for a value
- * in a sorted array of integers
+ * jump_search - searches for a value in a sorted array of integers
+ * using the Jump search algorithm
  *
  * @array: a pointer to the first element of the array to search in
- * @size:  the number of elements in array
- * @value: is the value to search for
- * Return: the first index where value is located
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return:  first index where value is located or -1 if value is not
+ * present in array or if array is NULL
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, step, jump;
+	size_t jump_size, i, target_idx;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
 
-	step = sqrt(size);
+	/*find jump size*/
+	jump_size = sqrt(size);
 
-	for (i = jump = 0; jump < size && array[jump] < value;)
+	/*find jump block containing target value*/
+	i = 0;
+	while (i < size && array[i] < value)
 	{
-		printf("Value checked array [%ld] = [%d]\n", jump, array[jump]);
-		i = jump;
-		jump += step;
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+		if (array[i] == value)
+		{
+			printf("Value found between indexes [%d] and [%d]\n",
+			       (int)(i - jump_size), (int)i);
+			return (i);
+		}
+		i = i + jump_size;
 	}
 
-	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+	/*perform linear search in block*/
+	if (i >= size || array[i] >= value)
+	{
+		printf("Value found between indexes [%d] and [%d]\n",
+		       (int)(i - jump_size), (int)i);
+		target_idx = jump_linear_search(array, i + 1, size,
+						jump_size, value);
+		return (target_idx);
+	}
+	return (-1);
+}
 
-	jump = jump > size - 1 ? jump : size - 1;
-	for (; i < jump && array[i] < value; i++)
-		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
-	printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+/**
+ * linear_search - searches for a value in an array of integers using the
+ * Linear search algorithm
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @start: index of upper end of jump block + 1
+ * @size: number of elements in array from breakpoint
+ * @jump_size: window size when interating through array
+ * @value: value to search for
+ * Return:  first index where value is located or -1 if value is not present
+ * in array or if array is NULL
+ */
 
-	return (array[i] == value ? (int)i : -1);
+
+int jump_linear_search(int *array, size_t start, size_t size,
+		       size_t jump_size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = start - jump_size - 1; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
